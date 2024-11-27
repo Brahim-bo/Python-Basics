9d2c382e3aad1bacd6e8fe339b7c2fd5f62d450b
Brahim-bo <bouskerzbrahim82@gmail.com>
2024-11-27 23:59:52 +0100

    firste py

---
+++ python.py
@@ -0,0 +1,9 @@
+for i in range(0 , 50 +1):
+    print("Brahim")
+   
+for i in range(1000 , 9555 , 10):
+    print(i +5)
+
+name= "BRAHIM BOUSKERZ"
+for i in range(len(name)):
+    print(name [i] ,len(name) , end="\n")
